bd4c74fe73c8d9bc3bee55daa08ee9cca815f147
chathuniM <as20240489@sci.sjp.ac.lk>
2025-10-24 15:07:17 +0530

    Initial commit

--- Logistics Management System/main.c
+++ Logistics Management System/main.c
@@ -1,1 +1,15 @@
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <float.h>
+
+#define MAX_CITIES 30
+#define MAX_DELIVERIES 50
+#define INF 999999
+#define FUEL_PRICE 310.0
+
+int main(){
+printf("Logistics Management System\n");
+return 0;
+}
