8494b9028d5234f03a25808f7716cc93b6a5e268
chathuniM <as20240489@sci.sjp.ac.lk>
2025-10-24 18:50:29 +0530

    Add vehicles and Delivery structs,globals and arrays

--- Logistics Management System/main.c
+++ Logistics Management System/main.c
@@ -13,3 +13,41 @@ int main(){
 printf("Logistics Management System\n");
 return 0;
 }
+struct Vehicle {
+    char name[10];
+    int capacity;
+    int rate_per_km;
+    int avg_speed;
+    int fuel_eff;
+};
+
+
+struct Delivery {
+    int source;
+    int dest;
+    int weight;
+    int vehicle_type;
+    float min_distance;
+    float delivery_cost;
+    float fuel_used;
+    float fuel_cost;
+    float total_cost;
+    float profit;
+    float customer_charge;
+    float time;
+};
+
+
+char cities[MAX_CITIES][50];
+int num_cities = 0;
+int distance[MAX_CITIES][MAX_CITIES];
+int min_dist[MAX_CITIES][MAX_CITIES];
+struct Vehicle vehicles[3] = {
+    {"Van", 1000, 30, 60, 12},
+    {"Truck", 5000, 40, 50, 6},
+    {"Lorry", 10000, 80, 45, 4}
+};
+
+struct Delivery deliveries[MAX_DELIVERIES];
+int num_deliveries = 0;
+
